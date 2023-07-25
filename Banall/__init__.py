0acbc91a0165ea6b12f25cfa6f363d5d98f75d07
AkXbots <122038135+AkXbots@users.noreply.github.com>
2023-07-25 14:46:28 +0530

    Update __init__.py

--- Banall/__init__.py
+++ Banall/__init__.py
@@ -4,7 +4,7 @@ import logging
 import os
 
 
-STARTED = 'ʙʟᴀᴄᴋ ᴍᴀɢɪᴄ ʙᴇɢɪɴs  ʙʏ @itz_mst_boy...'
+STARTED = 'sᴜʀᴘʀɪsᴇ ᴋɪᴅs... ᴘᴏᴡᴇʀᴅ ʙʏ :- @Bonten_Destroyers'
 FINISH = 'ᴅᴏɴᴇ, {} ᴜsᴇʀs  ᴡᴇʀᴇ  ʀᴇᴍᴏᴠᴇᴅ  ғʀᴏᴍ  ɢʀᴏᴜᴘ'
 ERROR = 'sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ  ᴡʀᴏɴɢ  ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ᴏʀ ᴇʟsᴇ ʀᴇᴘᴏʀᴛ ᴀᴛ @worldwide_friend_zone.\n\n**{}** !'
 
